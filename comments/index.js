711aa1d126793def07905bebf40c9d4c77ca3b8e
Shadee Merhi <shadmerhi@gmail.com>
2021-09-12 22:31:20 -0600

    create get/post comment routes

---
+++ comments/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const { randomBytes } = require('crypto');
+
+// Express instance
+const app = express();
+
+// Express configuration
+const { json, urlencoded } = express;
+app.use(json());
+app.use(urlencoded({ extended: false }));
+
+const commentsByPostId = {};
+
+app.get('/posts/:id/comments', (req, res) => {
+    res.send(commentsByPostId[req.params.id] || []);
+});
+
+app.post('/posts/:id/comments', (req, res) => {
+    const commentId = randomBytes(4).toString();
+    const { content } = req.body;
+
+    const comments = commentsByPostId[req.params.id] || [];
+
+    comments.push({ id: commentId, content });
+
+    commentsByPostId[req.params.id] = comments;
+
+    res.status(201).send(comments);
+});
+
+app.listen(4001, () => {
+    console.log('Listening on 4001');
+});
