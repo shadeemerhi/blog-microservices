2a173d42f2c1bc1c58bcd9370996ceb7d8be9e0e
Shadee Merhi <shadmerhi@gmail.com>
2021-09-13 23:14:01 -0600

    react app setup

--- comments/index.js
+++ comments/index.js
@@ -16,7 +16,7 @@ app.get('/posts/:id/comments', (req, res) => {
 });
 
 app.post('/posts/:id/comments', (req, res) => {
-    const commentId = randomBytes(4).toString();
+    const commentId = randomBytes(4).toString('hex');
     const { content } = req.body;
 
     const comments = commentsByPostId[req.params.id] || [];
